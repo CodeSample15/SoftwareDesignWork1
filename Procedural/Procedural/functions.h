41683de6c0975c2d2c1fc1f99224e33baf7efb75
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2021-02-05 10:45:53 -0500

    Update functions.h

--- Procedural/Procedural/functions.h
+++ Procedural/Procedural/functions.h
@@ -6,4 +6,11 @@
 
 #pragma once
 #include <string>
+#include <vector>
 
+void spacing(void); //to make cleaner output
+
+std::vector<std::string> getName(void); //will return an array of a first and last name
+std::vector<int> getGrades(void); //will return an int vector with all of the user's grades
+bool hasDisciplinaryIssue(void); //has a 5% chance of giving the user a disciplinary issue and returning true
+void printResults(std::string* name, std::vector<int> grades, bool disciplinaryIssue); //will print a formatted output of the user's input and will tell them if they got into honors or not
